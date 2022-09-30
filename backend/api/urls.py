378e59924ba83e64d2a607211cb2a4298eeed8da
Ivan Arkhipov <vanchez666@gmail.com>
2022-09-30 21:42:50 +0300

    cors whitelist

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -13,5 +13,5 @@ router.register('users', CustomUserViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('', include('djoser.urls')),
-    path('', include('djoser.urls.authtoken'))
+    path('auth/', include('djoser.urls.authtoken'))
 ]
--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -119,11 +119,11 @@ DJOSER = {
      },
 }
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = False
 
-# CORS_ORIGIN_WHITELIST = (
-#        'http://localhost:8000',
-# )
+CORS_ORIGIN_WHITELIST = (
+       'http://localhost:3000',
+)
 
 LANGUAGE_CODE = 'ru'
 
