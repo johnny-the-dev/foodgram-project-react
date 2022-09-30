ac2d929797c66a605729d864a0df1b3c6cf49d3e
Ivan Arkhipov <vanchez666@gmail.com>
2022-09-30 14:36:02 +0300

    added requirements

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -129,11 +129,11 @@ DJOSER = {
      },
 }
 
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
 
-CORS_ORIGIN_WHITELIST = (
-       'http://localhost:8000',
-)
+# CORS_ORIGIN_WHITELIST = (
+#        'http://localhost:8000',
+# )
 
 LANGUAGE_CODE = 'ru'
 
