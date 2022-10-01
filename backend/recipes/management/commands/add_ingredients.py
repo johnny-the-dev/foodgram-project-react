53fb7ffaa0f16b226c58737a6439f75b7085872a
Ivan Arkhipov <vanchez666@gmail.com>
2022-10-01 21:30:08 +0300

    command for ingredients and docker

---
+++ backend/recipes/management/commands/add_ingredients.py
@@ -0,0 +1,25 @@
+from django.core.management.base import BaseCommand
+import json
+
+from recipes.models import Ingredient
+
+class Command(BaseCommand):
+    help = 'Adds ingredients from json file'
+
+    def handle(self, *args, **options):
+        try:
+            with open('../data/ingredients.json', 'r') as file:
+                ingredients = json.load(file)
+                count_new = 0
+                for ingredient in ingredients:
+                    name = ingredient.get('name')
+                    measurement_unit = ingredient.get('measurement_unit')
+                    if measurement_unit and name:
+                        new, status = Ingredient.objects.get_or_create(name=name)
+                        if status:
+                            new.measurement_unit = measurement_unit
+                            count_new += 1
+                            new.save()                                                                      
+                print(f'Добавлено ингредиентов: {count_new}')
+        except FileNotFoundError as e:
+            print(e)
